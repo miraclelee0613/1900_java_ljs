227e04d5e17821249c763def86a64e28073fc389
miraclelee0613 <miraclelee0613@gmail.com>
2022-06-29 01:39:31 +0900

    ForTask_01:39

---
+++ day07/src/day07/SwitchTest.java
@@ -0,0 +1,27 @@
+package day07;
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		
+		switch(num) {
+		case 5:
+			System.out.println("5입니다");
+			break;
+		case 10:
+			System.out.println("10입니다");
+			break;
+		case 1:
+			System.out.println("1입니다");
+			break;
+		default:
+			System.out.println("default 입니다.");
+//			break;
+		}
+		
+
+	}
+
+}

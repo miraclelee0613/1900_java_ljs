a03279d03bde21d1115ca3cbae89eceb75d724a1
miraclelee0613 <miraclelee0613@gmail.com>
2022-07-02 21:16:14 +0900

    update?

--- myhome/src/myhome/WhileTask1.java
+++ myhome/src/myhome/WhileTask1.java
@@ -7,9 +7,29 @@ public class WhileTask1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 //		1. 코인노래방프로그램
-
+		
+		
+		
+		
+		
+		
 //		2. 키오스크프로그램
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 //		3. ATM프로그램
 		int choice = 0;
 		int money = 0;
